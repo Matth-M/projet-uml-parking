f1ce8700fd48a3e91f5827620a378d51a0b5f6e8
Matth-M <matthieu.matsima@gmail.com>
2023-02-18 22:16:05 +0100

    Print number of available places to the console

--- Parking.cpp
+++ Parking.cpp
@@ -1,12 +1,18 @@
 #include "Parking.h"
 #include "User.h"
+#include <iostream>
 #include <vector>
 Parking::Parking(int total_places) {
   this->total_places = total_places;
   this->occupied_places = 0;
 }
 
-void Parking::print_n_places(int occupied_places, int total_places) { return; }
+void Parking::print_n_places(int occupied_places, int total_places) {
+  int number = total_places - occupied_places;
+  // La console fait office de l'ecran du parking
+  std::cout << "PLACES DISPONIBLES:\t" << number << std::endl;
+}
+
 bool Parking::authorized_plate(std::string plate) const {
   for (int i = 0; i < list_users.size(); i++) {
     if (list_users[i].get_plate() == plate) {
