ff363265d7ea03fb284412777a77dd09e78119da
Matth-M <matthieu.matsima@gmail.com>
2023-02-19 00:34:54 +0100

    Add add/remove_present_user methods to Parking

--- Parking.cpp
+++ Parking.cpp
@@ -31,3 +31,26 @@ bool Parking::authorized_badge(int badge) const {
   }
   return false;
 }
+
+void Parking::remove_present_user(User u) {
+  // On cherche la position de l'utilisateur dans la la liste des utilisateurs
+  // presents pour l'effacer de la liste
+  for (int i = 0; i < list_of_present_cars.size(); i++) {
+    if (list_of_present_cars[i].get_badge() == u.get_badge() ||
+        list_of_present_cars[i].get_plate() == u.get_plate()) {
+      list_of_present_cars.erase(list_of_present_cars.begin() + i);
+    }
+  }
+}
+
+void Parking::add_present_user(User u) {
+  // On verifie si u est deja dans le parking
+  // S'il est deja present, on ne le rajoute pas dans la liste
+  for (int i = 0; i < list_of_present_cars.size(); i++) {
+    if (list_of_present_cars[i].get_badge() == u.get_badge() ||
+        list_of_present_cars[i].get_plate() == u.get_plate()) {
+      return;
+    }
+  }
+  list_of_present_cars.push_back(u);
+}
