0d07a823223f131a11681b396251e528efe95a33
Matth-M <matthieu.matsima@gmail.com>
2023-02-18 22:35:34 +0100

    Init du timer dans main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 const int TOTAL_PLACES = 100;
 
 int main() {
+  // Le timer est utilise pour compter le nombre de voitures presentes
+  // toutes les 30mn ou alors 5mn apres l'entree/sortie d'un
+  // vehicule.
+  int timer = 0;
   Parking parking(TOTAL_PLACES);
+
   return 0;
 }
